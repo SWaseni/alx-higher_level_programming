05b823e949a1eba3e506c3624ca171137caa936b
SWaseni <91959994+SWaseni@users.noreply.github.com>
2022-09-22 23:22:01 +0300

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Gets the size of the body of a response from a URL
-curl -sI "$1" grep -oiE 'Content-Length: [0-9]+' cut -d ' ' -f2
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
