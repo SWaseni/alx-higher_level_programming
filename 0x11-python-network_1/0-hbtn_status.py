64a8a873149f806ab8f78ce21fbb69c7d6364e42
SWaseni <91959994+SWaseni@users.noreply.github.com>
2022-09-24 20:42:06 +0300

    status updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Fetch url"""
+"""Fetches a given URL"""
 import urllib.request as request
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        if response.readable():
-            data = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(data)))
-            print("\t- content: {}".format(data))
-            print("\t- utf8 content: {}".format(data.decoder("utf-8"))https://intranet.hbtn.io/status)
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
